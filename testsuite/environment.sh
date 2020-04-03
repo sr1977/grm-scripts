5cfacb795160b7ddcce033784aef27501148c874
Steve Risdon <steve.risdon@skybettingandgaming.com>
2020-04-03 17:10:38 +0100

    Allowed streaming bets to an environment

--- testsuite/environment.sh
+++ testsuite/environment.sh
@@ -1,19 +1,102 @@
 function start_stream() {
    local type=${1:-orwell}
-   local bps=${2:-1}
-   local duration=${3:-15}
+   local env=${2:-local}
+   local bps=${3:-1}
+   local duration=${4:-15}
 
-   curl -X POST "http://localhost:1337/betStream?duration=${duration}&betSchema=${type}&betsPerSecond=${bps}"
+   case $env in
+       test) 
+           echo "Ensure you're auth'd into the cluster and are port forwarding 1337"
+           local testsuite_endpoint="http://localhost:1337/api/v1/namespaces/grm-adaptor-test/services/testsuite:1337/proxy/betStream"
+           ;;
+       stage)
+           echo "Ensure you're auth'd into the cluster and are port forwarding 1337"
+           local testsuite_endpoint="http://localhost:1337/api/v1/namespaces/grm-adaptor-stage/services/testsuite:1337/proxy/betStream"
+           ;;
+       *)
+           local testsuite_endpoint="http://localhost:1337/betStream"
+   esac
+
+   echo "Posting to env: $env"
+   curl -X POST "${testsuite_endpoint}?duration=${duration}&betSchema=${type}&betsPerSecond=${bps}"
+   echo "" # Adds missing newline from above
+}
+
+function port_forward_testsuite() {
+    kubectl proxy --port 1337
 }
 
 function stream_orwell() {
-   start_stream orwell $@
+     OPTIND=1
+
+    local options=":st"
+
+    while getopts $options opt; do
+        case $opt in
+            s)
+                local env=stage
+                ;;
+            t)
+                local env=test
+                ;;
+            \?)
+                echo "Unknown param - $options"
+                return 2
+                ;;
+        esac
+    done
+
+    shift $((OPTIND - 1))
+
+    start_stream orwell ${env:-local} $@
 }
 
 function stream_openbet() {
-   start_stream openbetEventsRaw $@
+     OPTIND=1
+
+    local options=":st"
+
+    while getopts $options opt; do
+        case $opt in
+            s)
+                local env=stage
+                ;;
+            t)
+                local env=test
+                ;;
+            \?)
+                echo "Unknown param - $options"
+                return 2
+                ;;
+        esac
+    done
+
+    shift $((OPTIND - 1))
+
+    start_stream openbetEventsRaw ${env:-local} $@
 }
 
 function stream_amelco() {
-   start_stream amelcoEvents $@
+     OPTIND=1
+
+    local options=":st"
+
+    while getopts $options opt; do
+        case $opt in
+            s)
+                local env=stage
+                ;;
+            t)
+                local env=test
+                ;;
+            \?)
+                echo "Unknown param - $options"
+                return 2
+                ;;
+        esac
+    done
+
+    shift $((OPTIND - 1))
+
+    start_stream amelcoEvents ${env:-local} $@
 }
