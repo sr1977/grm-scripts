e181715b790ac930eca0db1fd17635cb9caa4ce4
Steve Risdon <steve.risdon@skybettingandgaming.com>
2020-06-15 17:43:12 +0100

    Added new topics to reset to LQS;

--- kafka/streams/reset-queryservice-stage.sh
+++ kafka/streams/reset-queryservice-stage.sh
@@ -1,6 +1,6 @@
 kafka-streams-application-reset \
     --bootstrap-servers=gueaplatkafkatradingtst02.skybet.net:9093 \
     --application-id=gstp.risk.liabilities.queryservice.stage \
-    --input-topics=gstp.risk.liabilities.global.top.liablities.stage,gstp.risk.liabilities.bets.total.liabilities.stage,gstp.risk.liabilities.bets.total.liabilities.windowed.stage \
+    --input-topics=gstp.risk.liabilities.bets.total.liabilities.stage,gstp.risk.liabilities.bets.total.liabilities.windowed.stage,gstp.risk.liabilities.global.top.liablities.stage,gstp.risk.liabilities.global.windowed.liabilities.stage,gstp.risk.liabilities.queryservice.bysportsbook.stage,gstp.risk.liabilities.queryservice.bysportsbook.windowed.stage \
     --to-latest \
     --config-file /Users/sri01/workspace/grm-scripts/kafka/keystores/liabilityqueryservice-stage.properties
